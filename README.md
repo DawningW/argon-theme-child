9dbb6a7a3d3b939888cb8e6d33d677604d077506
Chen Wu <201436728@qq.com>
2022-03-10 20:22:43 +0800

    Child theme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # argon-theme-child
-Argon主题的子主题
+一个自用的[Argon主题](https://github.com/solstice23/argon-theme)的子主题
+
+目前为页脚添加了WordPress的版权信息
+
+未来可能会添加访客统计, 音乐播放器, live2d模型展示等功能
+
+~~*咕咕咕*~~
+
+请到Release下载zip文件, 确保Argon主题正确安装后在 WordPress 后台 "主题" 页面上传并安装
---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+
+//function child_enqueue_parent_scripts() {
+//    wp_enqueue_style('parent', get_template_directory_uri().'/style.css');
+//}
+// add_action('wp_enqueue_scripts', 'child_enqueue_parent_scripts');
+
+// require_once(get_stylesheet_directory().'/my_included_file.php');
+?>
