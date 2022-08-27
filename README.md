8c786a962d9fcb046fc94a3f51d15c39d23e70c6
Chen Wu <201436728@qq.com>
2022-08-27 20:42:56 +0800

    Update

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # argon-theme-child
 一个自用的[Argon主题](https://github.com/solstice23/argon-theme)的子主题
 
-目前为页脚添加了WordPress的版权信息
+目前为页脚添加了WordPress的版权信息, 以及为了备案修改网页标题而不影响站点标题的功能
 
 未来可能会添加访客统计, 音乐播放器, live2d模型展示等功能
 
--- functions.php
+++ functions.php
@@ -6,4 +6,10 @@
 // add_action('wp_enqueue_scripts', 'child_enqueue_parent_scripts');
 
 // require_once(get_stylesheet_directory().'/my_included_file.php');
+
+function child_pre_get_document_title() {
+    return '记录生活的点点滴滴';
+}
+add_filter('pre_get_document_title', 'child_pre_get_document_title');
+
 ?>
